653306b90e61a5ce85032d947178c0d0f7247abf
LanDev01 <alnjr0001@outlook.com>
2024-08-02 19:07:42 -0300

    Feat: Componentizing button

---
+++ src/app/components/button/button.component.ts
@@ -0,0 +1,20 @@
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-button',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './button.component.html',
+  styleUrl: './button.component.scss',
+})
+export class ButtonComponent {
+  @Input() text: string = '';
+  @Input() color: string = '';
+
+  @Output() buttonClick = new EventEmitter();
+
+  onClick() {
+    this.buttonClick.emit();
+  }
+}
